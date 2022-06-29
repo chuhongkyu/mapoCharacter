222bd87b4369d46a52e3420beeaf0890bd8d3ea2
Mr.chu <chuhongkyu@gmail.com>
2022-06-29 09:41:14 +0900

    테스트

--- src/utils/mapoCharacterData.js
+++ src/utils/mapoCharacterData.js
@@ -30,7 +30,41 @@ export const Buddies_data = [
     subImg: [
       "/assets/sub_img/BY-A.png",
       "/assets/sub_img/BY.png",
-      "/assets/sub_img/BM-D2.png",
+      "/assets/sub_img/BY-A.png",
+    ],
+  },
+  {
+    id: 2,
+    character: "/assets/characters/DG.png",
+    name: "동건",
+    nickname: "마포 버디즈의 행동대장",
+    hashs: ["고양이", "스트릿 출신", "정의의 사도"],
+    description: [
+      "불의를 보면 참지 못하고 바로 잡아야하는 정의의 사도.",
+      "주변인들을 많이 도와주는 편.",
+      "강해보이는 성격과는 별개로 속마음은 따뜻하고 여리다.",
+    ],
+    subImg: [
+      "/assets/sub_img/DG-A.png",
+      "/assets/sub_img/DG-A2.png",
+      "/assets/sub_img/DG.png",
+    ],
+  },
+  {
+    id: 3,
+    character: "/assets/characters/MC.png",
+    name: "MC 커니",
+    nickname: "마포 버디즈의 행동대장",
+    hashs: ["고양이", "스트릿 출신", "정의의 사도"],
+    description: [
+      "불의를 보면 참지 못하고 바로 잡아야하는 정의의 사도.",
+      "주변인들을 많이 도와주는 편.",
+      "강해보이는 성격과는 별개로 속마음은 따뜻하고 여리다.",
+    ],
+    subImg: [
+      "/assets/sub_img/MC-A.png",
+      "/assets/sub_img/MC.png",
+      "/assets/sub_img/MC.png",
     ],
   },
 ];
