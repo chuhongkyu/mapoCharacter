474b43f8e2ab04ce483379302ca29c7457c3f30e
Mr.chu <chuhongkyu@gmail.com>
2022-07-07 09:54:54 +0900

    배경 패턴 제작

--- src/components/CreatePattern.js
+++ src/components/CreatePattern.js
@@ -1,31 +1,41 @@
-import { useEffect } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const env = process.env;
 env.PUBLIC_URL = env.PUBLIC_URL || "";
 
-const Pattern = styled.img`
-  width: 20px;
-  position: absolute;
-  z-index: 5;
-  display: flex;
+const Anim = keyframes`
+0%{
+  transform: translateY(0);
+}
+50%{
+  transform: translateY(-5px);
+}
+100%{
+  transform: translateY(0);
+}
 `;
 
-const CreatePattern = () => {
-  const randomPosition = () => {
-    Math.floor(Math.random() * 100);
-  };
-  const position = randomPosition() + "%";
-  useEffect(() => {
-    randomPosition();
-  }, []);
+const Pattern = styled.img`
+  width: 40px;
+  height: auto;
+  position: absolute;
+  z-index: 0;
+  display: flex;
+  animation: ${Anim} 4s ease-in-out infinite;
+  transition: 0.5s;
+`;
 
+const CreatePattern = ({ alt, icon }) => {
+  const randomX = Math.floor(Math.random() * 100);
+  const randomY = Math.floor(Math.random() * 200);
+  const positionX = randomX + "%";
+  const positionY = randomY + "%";
   return (
     <>
       <Pattern
-        style={{ marginLeft: position }}
-        src={env.PUBLIC_URL + "/assets/pattern/1.png"}
-        alt="1"
+        style={{ marginLeft: positionX, marginTop: positionY }}
+        src={env.PUBLIC_URL + icon}
+        alt={alt}
       />
     </>
   );
